cefc1cf4db7e06bb02a0f52038b728189e05f9c1
Tahirr02 <tahir.balcik@outlook.com>
2021-05-08 20:45:31 +0200

    methode isDatumCorrect aangepast zodat de test niet meer zou falen.

--- src/Maandsalaris.java
+++ src/Maandsalaris.java
@@ -1,14 +1,64 @@
+import java.time.LocalDate;
+
 public class Maandsalaris {
     private  int jaartal;
     private  int maand;
+    LocalDate date = LocalDate.now();
 
     public Maandsalaris(){
 
     }
 
+    private boolean isJaartalCorrect(int jaartal){
+        if(jaartal > 1900 && jaartal < 9999){
+            return true;
+
+        }
+        else{
+            return false;
+        }
+    }
+
+    private boolean isMaandCorrect(int maand){
+        if(maand >= 1 && maand < 13){
+            return true;
+
+        }
+        else {
+            return false;
+        }
+    }
+
 
     public boolean isDatumCorrect(){
-        return true;
+
+        if (isJaartalCorrect(this.jaartal) == false){
+            return false;
+        }
+        else if(isMaandCorrect(this.maand) == false){
+            return false;
+        }
+
+        else if(isDatumInVerleden() == false){
+            return false;
+        }
+        else {
+            return true;
+        }
+
+
+
+    }
+    private boolean isDatumInVerleden(){
+        if(this.jaartal < date.getYear()){
+            return true;
+        }
+        else if(jaartal == date.getYear() && maand < date.getMonthValue()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public void setJaartal(int jaartal){
@@ -17,5 +67,6 @@ public class Maandsalaris {
 
     public void setMaand(int maand){
         this.maand = maand;
+
     }
 }

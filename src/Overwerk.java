34126069e9c1f0e6083f99da30ecdc219942f29f
Tahirr02 <tahir.balcik@outlook.com>
2021-05-08 23:58:23 +0200

    opdracht pairwise testing
    logica geschreven voor methode getOverWerkPercentage. en de expected waarde van de eerste 2 j unit tests aangepast omdat deze fout waren.

--- src/Overwerk.java
+++ src/Overwerk.java
@@ -7,7 +7,33 @@ public class Overwerk {
     private static String FUNCTIE_BEDIENING = "bediening";
 
     public static int getOverWerkPerecntage(String soortOverwerk, Boolean oproepKracht, Boolean avondDienst, String functie){
-        int overwerkpercentage = -1;
+        int overwerkpercentage = 0;
+        if(soortOverwerk == DOORDEWEEKS){
+            overwerkpercentage += 20;
+        }
+        else if(soortOverwerk == WEEKEND){
+            overwerkpercentage += 50;
+        }
+        else if(soortOverwerk == FEESTDAG){
+
+            overwerkpercentage += 100;
+        }
+
+        if(oproepKracht == true){
+            overwerkpercentage += 10;
+        }
+
+        if(avondDienst == true){
+            overwerkpercentage += 20;
+        }
+
+        if(functie == FUNCTIE_KOK){
+            overwerkpercentage += 30;
+        }
+        else if(functie == FUNCTIE_BEDIENING){
+            overwerkpercentage += 15;
+        }
+
 
         return overwerkpercentage;
     }
--- src/OverwerkTest.java
+++ src/OverwerkTest.java
@@ -6,8 +6,8 @@ class OverwerkTest {
 
     @Test
     void getOverWerkPercentage() {
-        assertEquals (60, Overwerk.getOverWerkPerecntage ("doordeweeks", true, true, "kok"));
-        assertEquals (15, Overwerk.getOverWerkPerecntage ("doordeweeks", false, false, "bediening"));
+        assertEquals (80, Overwerk.getOverWerkPerecntage ("doordeweeks", true, true, "kok"));
+        assertEquals (35, Overwerk.getOverWerkPerecntage ("doordeweeks", false, false, "bediening"));
         assertEquals (90, Overwerk.getOverWerkPerecntage ("weekend", true, false, "kok"));
         assertEquals (85, Overwerk.getOverWerkPerecntage ("weekend", false, true, "bediening"));
         assertEquals (125, Overwerk.getOverWerkPerecntage ("feestdag", true, false, "bediening"));
